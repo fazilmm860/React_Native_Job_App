3b7e57a7931d5e16c10060df1f3f932e33355c59
Mohamed Fazil M <fazilmm860@gmail.com>
2023-08-23 08:56:52 +0530

    inital commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hello WOrld</Text>
+        </View>
+    )
+}
+
+export default Home;
