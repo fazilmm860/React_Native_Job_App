3cea0237d7fcf0452dd58ffb91a2db724b627e3b
Mohamed Fazil M <fazilmm860@gmail.com>
2023-08-23 09:20:59 +0530

    folder structure of app

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 const Home = () => {
     return (
         <View>
-            <Text>Hello WOrld</Text>
+            <Text>Home</Text>
         </View>
     )
 }
